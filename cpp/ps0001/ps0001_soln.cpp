3ebbb0fb1bda43fa3f9b902af3682a221e386048
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-03 10:19:23 +0300

    problem 1 marked

---
+++ cpp/ps0001/ps0001_soln.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // prompting user for 3 numers to add 1/2
+  unsigned short num1;
+  cout << "enter value of a " <<endl;
+  cin >> num1;
+  unsigned short num2;
+  cout << "enter value of b " <<endl;
+  cin >> num2;
+  unsigned short num3;
+  cout << "enter value of c " <<endl;
+  cin >> num3;
+
+  // outputting the result of the summtion process 3/3
+  unsigned short sum = num1 + num2 + num3;
+  cout << "sum= " << sum << endl;
+  
+  return 0;
+}
+// great work 😎 8/10 ⭐
